64926a5f563e69e90ce357ae11de7ca99e45a122
Arjav <1971arjavbarya@gmail.com>
2024-07-31 12:11:12 +0530

    shifted database to atlas

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 
 const dbConnect=()=>{
     try{
-        const conn = mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+        // const conn = mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+        const conn = mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@devhub.pyuqovm.mongodb.net/?retryWrites=true&w=majority&appName=Devhub`,);
         console.log("Database Connected Successfully")
     }
     catch(error){
